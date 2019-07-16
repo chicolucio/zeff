a92f93350571fbdc4cd974b73e86ba164743b735
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2019-07-16 16:26:18 -0300

    Plot labels translated to English. Functions docstrings

--- zeff.py
+++ zeff.py
@@ -122,7 +122,7 @@ def elem_data(name):
 
     Returns
     -------
-    Pandas frame.
+    Pandas DataFrame.
         Summary of Zeff and S for a given element with Slater and Clementi
         screening values.
     """
@@ -148,6 +148,20 @@ def elem_data(name):
 
 
 def plot_param(ax=None):
+    """Common plot parameters.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes
+        Axes for the plot (the default is None).
+
+    Returns
+    -------
+    None
+        Should be called inside a plot function in order to apply the
+        parameters.
+
+    """
     ax.axhline(color='gray', zorder=-1)
     ax.axvline(color='gray', zorder=-1)
     ax.grid(b=True, which='major', linestyle=':', linewidth=2)
@@ -157,64 +171,160 @@ def plot_param(ax=None):
 
 
 def plot_slater_zef(name, ax=None, **kwargs):
+    """Plots effective nuclear charge (Slater) per orbital.
+
+    Parameters
+    ----------
+    name : string
+        String with the element symbol.
+    ax : matplotlib.axes
+        Axes for the plot (the default is None).
+    **kwargs : string
+        kwargs for plot parameters.
+
+    Returns
+    -------
+    None
+        Plot.
+
+    """
     plot_param(ax)
+    fontsize = 15
+    linewidth = 4
     x = elem_data(name)['Orbital']
     y = elem_data(name)['Zef Slater']
-    ax.set_ylabel('Carga nuclear efetiva', size=15)
-    ax.set_xlabel('Orbitais', size=15)
+    ax.set_ylabel('Effective nuclear charge', size=fontsize)
+    ax.set_xlabel('Orbitals', size=fontsize)
     ax.set_xticks = ([i for i in range(len(elem_data(name).index))])
     ax.set_yticks(np.arange(0, round(max(y)) + 5, 5.0))
-    ax.plot(x, y, linewidth=4, label='Zef Slater {0}'.format(
+    ax.plot(x, y, linewidth=linewidth, label='Zef Slater {0}'.format(
         element(name).symbol), **kwargs)
-    ax.legend(fontsize=14, loc='upper right', shadow=True, fancybox=True)
+    ax.legend(fontsize=fontsize - 1, loc='best', shadow=False, fancybox=True,
+              bbox_to_anchor=(1, 1))
 
 
 def plot_slater_screening(name, ax=None, **kwargs):
+    """Plots Shielding /% (Slater) per orbital.
+
+    Parameters
+    ----------
+    name : string
+        String with the element symbol.
+    ax : matplotlib.axes
+        Axes for the plot (the default is None).
+    **kwargs : string
+        kwargs for plot parameters.
+
+    Returns
+    -------
+    None
+        Plot.
+
+    """
     plot_param(ax)
+    fontsize = 15
+    linewidth = 4
     x = elem_data(name)['Orbital']
     y = elem_data(name)['% S Slater']
-    ax.set_ylabel('Blindagem / %', size=15)
-    ax.set_xlabel('Orbitais', size=15)
+    ax.set_ylabel('Shielding / %', size=fontsize)
+    ax.set_xlabel('Orbitals', size=fontsize)
     ax.set_xticks = ([i for i in range(len(elem_data(name).index))])
-    ax.set_yticks(np.arange(0, round(max(y)) + 5, 5.0))
-    ax.plot(x, y, linewidth=4, label='Blindagem % Slater {0}'.format(
+    ax.set_yticks(np.arange(0, round(max(y)) + 5, 10.0))
+    ax.plot(x, y, linewidth=linewidth, label='Shielding % Slater {0}'.format(
         element(name).symbol), **kwargs)
-    ax.legend(fontsize=13, loc='best', shadow=True, fancybox=True)
+    ax.legend(fontsize=fontsize - 2, loc='best', shadow=True, fancybox=True)
 
 
 def plot_clementi_zef(name, ax=None, **kwargs):
+    """Plots effective nuclear charge (Clementi) per orbital.
+
+    Parameters
+    ----------
+    name : string
+        String with the element symbol.
+    ax : matplotlib.axes
+        Axes for the plot (the default is None).
+    **kwargs : string
+        kwargs for plot parameters.
+
+    Returns
+    -------
+    None
+        Plot.
+
+    """
     plot_param(ax)
+    fontsize = 15
+    linewidth = 4
     x = elem_data(name)['Orbital']
     y = elem_data(name)['Zef Clementi']
-    ax.set_ylabel('Carga nuclear efetiva', size=15)
-    ax.set_xlabel('Orbitais', size=15)
+    ax.set_ylabel('Effective nuclear charge', size=fontsize)
+    ax.set_xlabel('Orbitals', size=fontsize)
     ax.set_xticks = ([i for i in range(len(elem_data(name).index))])
     ax.set_yticks(np.arange(0, round(max(y)) + 5, 5.0))
-    ax.plot(x, y, linewidth=4, label='Zef Clementi {0}'.format(
+    ax.plot(x, y, linewidth=linewidth, label='Zef Clementi {0}'.format(
         element(name).symbol), **kwargs)
-    ax.legend(fontsize=14, loc='upper right', shadow=True, fancybox=True)
+    ax.legend(fontsize=fontsize - 1, loc='upper right',
+              shadow=True, fancybox=True)
 
 
 def plot_clementi_screening(name, ax=None, **kwargs):
+    """Plots Shielding /% (Clementi) per orbital.
+
+    Parameters
+    ----------
+    name : string
+        String with the element symbol.
+    ax : matplotlib.axes
+        Axes for the plot (the default is None).
+    **kwargs : string
+        kwargs for plot parameters.
+
+    Returns
+    -------
+    None
+        Plot.
+
+    """
     plot_param(ax)
+    fontsize = 15
+    linewidth = 4
     x = elem_data(name)['Orbital']
     y = elem_data(name)['% S Clementi']
-    ax.set_ylabel('Blindagem / %', size=15)
-    ax.set_xlabel('Orbitais', size=15)
+    ax.set_ylabel('Shielding / %', size=fontsize)
+    ax.set_xlabel('Orbitals', size=fontsize)
     ax.set_xticks = ([i for i in range(len(elem_data(name).index))])
-    ax.set_yticks(np.arange(0, round(max(y)) + 5, 5.0))
-    ax.plot(x, y, linewidth=4, label='Blindagem % Clementi {0}'.format(
+    ax.set_yticks(np.arange(0, round(max(y)) + 5, 10.0))
+    ax.plot(x, y, linewidth=linewidth, label='Shielding % Clementi {0}'.format(
         element(name).symbol), **kwargs)
-    ax.legend(fontsize=14, loc='lower right', shadow=True, fancybox=True)
+    ax.legend(fontsize=fontsize - 1, loc='lower right',
+              shadow=True, fancybox=True)
 
 
 def plot_slater_both(name, ax=None):
+    """Plots Effective nuclear charge and shielding /% (Slater) per orbital.
+
+    Parameters
+    ----------
+    name : string
+        String with the element symbol.
+    ax : matplotlib.axes
+        Axes for the plot (the default is None).
+
+    Returns
+    -------
+    None
+        Plot.
+
+    """
     plot_param(ax)
+    fontsize = 15
+    linewidth = 4
     x = elem_data(name)['Orbital']
     y = elem_data(name)['Zef Slater']
-    ax.set_ylabel('Carga nuclear efetiva', size=15, color='blue')
-    ax.set_xlabel('Orbitais', size=15)
-    line1 = ax.plot(x, y, linewidth=4, label='Zef Slater {0}'.format(
+    ax.set_ylabel('Effective nuclear charge', size=fontsize, color='blue')
+    ax.set_xlabel('Orbitals', size=fontsize)
+    line1 = ax.plot(x, y, linewidth=linewidth, label='Zef Slater {0}'.format(
         element(name).symbol), color='blue')
     ax.set_xticks = ([i for i in range(len(elem_data(name).index))])
     ax.set_yticks(np.linspace(0, round(ax.get_ybound()[1] + 1), 5))
@@ -222,39 +332,57 @@ def plot_slater_both(name, ax=None):
     ax2 = ax.twinx()
     plot_param(ax2)
     y2 = elem_data(name)['% S Slater']
-    line2 = ax2.plot(x, y2, linewidth=4, label='Blindagem % Slater {0}'.format(
-        element(name).symbol), color='red')
-    ax2.set_ylabel('Blindagem / %', size=15, color='red')
+    line2 = ax2.plot(x, y2, linewidth=linewidth, linestyle='--',
+                     label='Shielding % Slater {0}'.format(element(name).symbol), color='red')
+    ax2.set_ylabel('Shielding / %', size=fontsize, color='red')
     ax2.set_yticks(np.linspace(0, round(ax2.get_ybound()[1] + 1), 5))
 
     lines = line1 + line2
     labels = [l.get_label() for l in lines]
 
-    ax2.legend(lines, labels, fontsize=14,
+    ax2.legend(lines, labels, fontsize=fontsize - 1,
                loc='upper center', shadow=True, fancybox=True)
 
 
 def plot_clementi_both(name, ax=None):
+    """Plots Effective nuclear charge and shielding /% (Clementi) per orbital.
+
+    Parameters
+    ----------
+    name : string
+        String with the element symbol.
+    ax : matplotlib.axes
+        Axes for the plot (the default is None).
+
+    Returns
+    -------
+    None
+        Plot.
+
+    """
     plot_param(ax)
+    fontsize = 15
+    linewidth = 4
     x = elem_data(name)['Orbital']
     y = elem_data(name)['Zef Clementi']
-    ax.set_ylabel('Carga nuclear efetiva', size=15, color='blue')
-    ax.set_xlabel('Orbitais', size=15)
-    line1 = ax.plot(x, y, linewidth=4, label='Zef Clementi {0}'.format(
-        element(name).symbol), color='blue')
+    ax.set_ylabel('Effective nuclear charge', size=fontsize, color='blue')
+    ax.set_xlabel('Orbitals', size=fontsize)
+    line1 = ax.plot(x, y, linewidth=linewidth,
+                    label='Zef Clementi {0}'.format(element(name).symbol),
+                    color='blue')
     ax.set_xticks = ([i for i in range(len(elem_data(name).index))])
     ax.set_yticks(np.linspace(0, round(ax.get_ybound()[1] + 1), 5))
 
     ax2 = ax.twinx()
     plot_param(ax2)
     y2 = elem_data(name)['% S Clementi']
-    line2 = ax2.plot(x, y2, linewidth=4, label='Blindagem % Clementi {0}'.format(
-        element(name).symbol), color='red')
-    ax2.set_ylabel('Blindagem / %', size=15, color='red')
+    line2 = ax2.plot(x, y2, linewidth=linewidth, linestyle='--',
+                     label='Shielding % Clementi {0}'.format(element(name).symbol), color='red')
+    ax2.set_ylabel('Shielding / %', size=fontsize, color='red')
     ax2.set_yticks(np.linspace(0, round(ax2.get_ybound()[1] + 1), 5))
 
     lines = line1 + line2
     labels = [l.get_label() for l in lines]
 
-    ax2.legend(lines, labels, fontsize=14,
+    ax2.legend(lines, labels, fontsize=fontsize - 1,
                loc='upper center', shadow=True, fancybox=True)
